294550dbe1cf0561ebb655f213a6c2f4604244ab
“karandepale” <karandepale111@gmail.com>
2023-05-18 10:15:14 -0700

    Add:Solving using Switch Case Statement

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EmployeeWageComputation
 {
@@ -11,37 +7,34 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+            // IMPLEMENTTAION OF SWITCH CASE STATEMENT
+            Console.WriteLine("------------- Implementing using Swich case Statement ------------------");
 
             // Checking Employee is present or absent
             Random obj = new Random();
             int randomVal = obj.Next(0, 2);
-            Console.WriteLine("Random value : " + randomVal);
+            Console.WriteLine("Random value: " + randomVal);
 
-            if(randomVal == 0)
+            switch (randomVal)
             {
-                Console.WriteLine("Employee is Absent...");
+                case 0:
+                    Console.WriteLine("Employee is Absent...");
+                    break;
+                case 1:
+                    Console.WriteLine("Employee is Present...");
+                    // Calculate Daily employee wage
+                    int wagePerHr = 20;
+                    int fullDayHr = 8;
+                    int DailyEmpWage = wagePerHr * fullDayHr;
+                    Console.WriteLine("Daily Employee wage is: " + DailyEmpWage);
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Employee is Present...");
-            }
 
-
-
-            // Calculate Daily employee wage
-            int wagePerHr = 20;
-            int fullDayHr = 8;
-
-            int DailyEmpWage = wagePerHr * fullDayHr;
-            Console.WriteLine("Daily Employee wage is: " + DailyEmpWage);
-
-
-
-
-            //calculate part time employee wage
+            // Calculate part-time employee wage
             int partTimeHr = 8;
-            int partTimeEmpWage = wagePerHr * partTimeHr;
-            Console.WriteLine("Part time Employee wage is: " + partTimeEmpWage);
+            int wagePerHr1 = 20;
+            int partTimeEmpWage = wagePerHr1 * partTimeHr;
+            Console.WriteLine("Part-time Employee wage is: " + partTimeEmpWage);
         }
     }
 }
