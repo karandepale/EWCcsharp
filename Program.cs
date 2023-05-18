a89853a9da46f51aa8d8633b139ee918b91c4ef7
“karandepale” <karandepale111@gmail.com>
2023-05-18 09:39:13 -0700

    Add: Check Employee is Present or Absent

--- Program.cs
+++ Program.cs
@@ -11,6 +11,20 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+            Random obj = new Random();
+            int randomVal = obj.Next(0, 2);
+            Console.WriteLine("Random value : " + randomVal);
+
+            if(randomVal == 0)
+            {
+                Console.WriteLine("Employee is Absent...");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Present...");
+            }
+
         }
     }
 }
